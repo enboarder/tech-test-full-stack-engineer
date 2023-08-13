e2a02b118502db6e79368b5638be857ae8a59c37
David Li <xxinli.1983@gmail.com>
2023-08-13 22:29:11 +1000

    wrong commit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,5 @@
 require('dotenv').config();
 
-const app = require('./config/express');
-const ship = require('./services/shipService');
-//const db = require('./config/mysql');
-
-//db.authenticate();
-ship.initialLoadData();
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port: ${process.env.PORT}, wait for the development server to be up...`);
